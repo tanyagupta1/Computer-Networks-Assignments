817e1074a9911ea2303c5f75cb60fd862a7b29ea
tanyagupta1 <tanya19119@iiitd.ac.in>
2021-11-23 13:57:03 +0530

    Q3

---
+++ A4/Q3/main.cpp
@@ -0,0 +1,49 @@
+#include "node.h"
+
+vector<RoutingNode*> distanceVectorNodes;
+
+void routingAlgo(vector<RoutingNode*> &distanceVectorNodes);
+
+int main() {
+    int n;
+    cin>>n;
+    string name;
+    distanceVectorNodes.clear();
+    for (int i = 0 ; i < n; i++) 
+	{
+      RoutingNode *newnode = new RoutingNode();
+      cin>>name;
+      newnode->setName(name);
+      distanceVectorNodes.push_back(newnode);
+    }
+    cin>>name;
+    while(name != "EOE") 
+	{
+		for (int i =0 ; i < distanceVectorNodes.size(); i++) 
+		{
+			string myeth,oeth, oname;
+			if(distanceVectorNodes[i]->getName() == name) 
+			{
+				cin>>myeth; 	
+				cin>>oeth;
+				cin>>oname;
+	  			for(int j = 0 ; j < distanceVectorNodes.size(); j++) 
+				  {
+					  if(distanceVectorNodes[j]->getName() == oname) 
+					  {
+						  distanceVectorNodes[i]->addInterface(myeth, oeth, distanceVectorNodes[j]);
+						  distanceVectorNodes[i]->addTblEntry(myeth, 0);
+						  break;
+	    			  }
+	  			  }
+			}
+    }
+      cin>>name;
+    }
+
+    /* The logic of the routing algorithm should go here */
+    routingAlgo(distanceVectorNodes);
+    /* Add the logic for periodic update (after every 1 sec) here */
+    
+}
+
---
+++ A4/Q3/routing_algo_c.cpp
@@ -0,0 +1,135 @@
+#include "node.h"
+#include <iostream>
+
+using namespace std;
+
+void printRT(vector<RoutingNode*> nd)
+{
+/*Print routing table entries*/
+	for (int i = 0; i < nd.size(); i++) 
+  {
+	  nd[i]->printTable();
+	}
+}
+
+void routingAlgo(vector<RoutingNode*> nd)
+{
+  //Your code here
+   printRT(nd);
+  int no_nodes = nd.size();
+  for(int i=0;i<no_nodes;i++)
+  {
+    nd[i]->sendMsg();
+  }
+  /*Print routing table entries after routing algo converges */
+  printRT(nd);
+
+  // part b
+  for(auto &e: nd)
+  {
+    if(e->getName()=="B") e->modify_Rtable("10.0.1.3",16);
+    if(e->getName()=="C") e->modify_Rtable("10.0.1.23",16);
+  }
+  cout<<"tables after increasing B-C link cost"<<'\n';
+  printRT(nd);
+  cout<<"starting updates"<<'\n';
+  for(int i=0;i<no_nodes;i++)
+  {
+    nd[i]->sendMsg();
+  }
+
+}
+
+
+void RoutingNode::recvMsg(RouteMsg *msg) 
+{
+
+  string from_ip = msg->from;
+  string to_ip = msg->recvip;
+  cout<<"processing msg from "<<from_ip<<" to "<<to_ip<<'\n';
+  vector<RoutingEntry> received_table = msg->mytbl->tbl;
+  bool modified = false;
+  int rcvd_size = received_table.size();
+  int cost_to_add = 0;
+  
+
+  for(int i=0;i<rcvd_size;i++)
+  {
+    string dest_ip = received_table[i].dstip;
+    string my_interface = received_table[i].ip_interface;
+    bool is_self_loop = dest_ip == my_interface;
+    int cost = received_table[i].cost+1;
+    bool found = false;
+
+    for( auto &e: this->mytbl.tbl)
+    {
+      if(is_self_loop) continue;
+      if(e.dstip==dest_ip)
+      {
+        found = true;
+        if(e.cost>cost)
+        {
+          e.cost = cost;
+          e.nexthop = from_ip;
+          e.ip_interface = to_ip;
+          modified = true;
+        }
+        else if(e.nexthop==from_ip)
+        {
+          if(e.cost!=cost)
+          {
+            e.cost = cost;
+            e.nexthop = from_ip;
+            e.ip_interface = to_ip;
+            modified = true;
+          }
+        }
+        break;
+      }
+    }
+    if(!found)
+    {
+      if(is_self_loop)
+      {
+        bool exists = false;
+        for(auto &e : this->mytbl.tbl)
+        {
+          if(e.dstip == dest_ip)
+          {
+            exists = true;
+            e.cost = 1;
+            e.ip_interface = to_ip;
+            e.nexthop = from_ip;
+            break;
+          }
+        }
+        if(!exists) 
+        {
+          modified = true;
+          this->addTblEntry(dest_ip,from_ip,to_ip,cost);
+        }
+      } 
+      else 
+      {
+        this->addTblEntry(dest_ip,from_ip,to_ip,cost);
+        modified = true;
+      }
+    }
+  }
+  if(modified)
+  {
+    cout<<"table modified"<<'\n';
+    printTable();
+    sendMsg();
+  }
+  else
+  {
+    cout<<"table not modified"<<'\n';
+  }
+  
+  //your code here
+}
+
+
+
+
